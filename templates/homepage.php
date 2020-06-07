f5317b9ae517cbc04cd6719fc3eb45687e475539
MrAnderson64 <mranderson64@live.com>
2020-06-07 22:28:21 +0100

    added % 4 check into while loop

--- templates/homepage.php
+++ templates/homepage.php
@@ -37,7 +37,6 @@ while($i < 4){
 </div></div>';
 $i++;
 }
-curl_close($ch);
 $middle .= '
     </div>
 	</div>
@@ -55,7 +54,29 @@ while($e < 27){
 	$nameObj = json_decode($return);
 	$name = $nameObj->{'name'};
 	$handle = str_replace(' ', '', $name);
-	$middle .= '<div class="column"><div class="card">
+	if($e % 4 == 0){
+		$middle .= '<div class="column"><div class="card">
+  <div class="card-image">
+    <figure class="image is-4by3">
+      <img src="' . $host . '/images/Rams/' . $e . '.jpg" alt="Placeholder image">
+    </figure>
+  </div>
+  <div class="card-content">
+    <div class="media">
+      <div class="media-left">
+        <figure class="image is-48x48">
+          <img src="' . $host . '/images/Rams/' . $e . '.jpg" alt="Placeholder image">
+        </figure>
+      </div>
+      <div class="media-content">
+        <p class="title is-4">' . $name . '</p>
+        <p class="subtitle is-6">@' . $handle . '</p>
+      </div>
+    </div>
+  </div>
+</div></div></div><div class="columns is-desktop">';
+	}else{
+		$middle .= '<div class="column"><div class="card">
   <div class="card-image">
     <figure class="image is-4by3">
       <img src="' . $host . '/images/Rams/' . $e . '.jpg" alt="Placeholder image">
@@ -75,6 +96,7 @@ while($e < 27){
     </div>
   </div>
 </div></div>';
+	}
 $e++;
 }
 curl_close($ch);
