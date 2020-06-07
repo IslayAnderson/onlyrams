56060111fda536a761e46275b327d69f98a034e4
MrAnderson64 <mranderson64@live.com>
2020-06-07 22:22:13 +0100

    Added Explore Rams Section

--- templates/homepage.php
+++ templates/homepage.php
@@ -3,7 +3,7 @@ $url = "https://api.namefake.com/random/male";
 $ch = curl_init();
 $top = '<section class="section">
     <div class="container">
-      <h1 class="title">featured Rams</h1>
+      <h1 class="title">Featured Rams</h1>
 	  <div class="columns is-desktop">';
 $i=0;
 $middle = '';
@@ -15,7 +15,6 @@ while($i < 4){
 	$o = rand(1, 27);
 	$name = $nameObj->{'name'};
 	$handle = str_replace(' ', '', $name);
-	$address = $nameObj->{'address'};
 	$middle .= '<div class="column"><div class="card">
   <div class="card-image">
     <figure class="image is-4by3">
@@ -27,6 +26,46 @@ while($i < 4){
       <div class="media-left">
         <figure class="image is-48x48">
           <img src="' . $host . '/images/Rams/' . $o . '.jpg" alt="Placeholder image">
+        </figure>
+      </div>
+      <div class="media-content">
+        <p class="title is-4">' . $name . '</p>
+        <p class="subtitle is-6">@' . $handle . '</p>
+      </div>
+    </div>
+  </div>
+</div></div>';
+$i++;
+}
+curl_close($ch);
+$middle .= '
+    </div>
+	</div>
+  </section>';
+  
+$e=0;
+$middle .= '<section class="section">
+    <div class="container">
+      <h1 class="title">Explore Rams</h1>
+	  <div class="columns is-desktop">';
+while($e < 27){
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	$return = curl_exec($ch);
+	$nameObj = json_decode($return);
+	$name = $nameObj->{'name'};
+	$handle = str_replace(' ', '', $name);
+	$middle .= '<div class="column"><div class="card">
+  <div class="card-image">
+    <figure class="image is-4by3">
+      <img src="' . $host . '/images/Rams/' . $e . '.jpg" alt="Placeholder image">
+    </figure>
+  </div>
+  <div class="card-content">
+    <div class="media">
+      <div class="media-left">
+        <figure class="image is-48x48">
+          <img src="' . $host . '/images/Rams/' . $e . '.jpg" alt="Placeholder image">
         </figure>
       </div>
       <div class="media-content">
