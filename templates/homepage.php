6593e77b89290a40f8b86d27d42133f312fece76
MrAnderson64 <mranderson64@live.com>
2020-07-13 09:14:29 +0100

    head empty

--- templates/homepage.php
+++ templates/homepage.php
@@ -12,6 +12,7 @@ $middle .= '<section class="section">
       <h1 class="title">Explore Rams</h1>
 	  <div class="columns is-desktop">';
 while($i < 100){
+	if($e % 4 == 0){
 	$middle .= '<div class="column"><a href="' . $xml2->sheep->url[$i] . '"><div class="card">
   <div class="card-image">
     <figure class="image is-4by3">
@@ -31,7 +32,29 @@ while($i < 100){
       </div>
     </div>
   </div>
+</div></div></a></div><div class="columns is-desktop">';
+}else{
+		$middle .= '<div class="column"><a href="' . $xml2->sheep->url[$i] . '"><div class="card">
+  <div class="card-image">
+    <figure class="image is-4by3">
+      <img src="' . $xml2->sheep->img[$i] . '" alt="Placeholder image">
+    </figure>
+  </div>
+  <div class="card-content">
+    <div class="media">
+      <div class="media-left">
+        <figure class="image is-48x48">
+          <img src="' . $xml2->sheep[$i]->img . '" alt="Placeholder image">
+        </figure>
+      </div>
+      <div class="media-content">
+        <p class="title is-4">' . $xml2->sheep->title[$i] . '</p>
+        <p class="subtitle is-6">' . $xml2->sheep->location[$i] . ', ' . $xml2->sheep->age[$i] . '</p>
+      </div>
+    </div>
+  </div>
 </div></div></a></div>';
+}
 	$i++;
 	}
 
