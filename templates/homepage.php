256e298996ad8b811fdf924aabdb040f07ef99b3
MrAnderson64 <mranderson64@live.com>
2020-07-13 11:05:41 +0100

    okay now

--- templates/homepage.php
+++ templates/homepage.php
@@ -1,7 +1,6 @@
 <?php 
 $xml2 = simplexml_load_file($host . "/data/json/rams.xml") or die("Error: Cannot create object");
 #$countXML = count($xml2->root->title[$i])
-echo $xml2;
 $top = '<section class="section">
     <div class="container">
       <h1 class="title">Explore Rams</h1>
@@ -11,23 +10,23 @@ $middle = '';
 while($i < 100){
 	if($i % 4 == 0){
 	$middle .= '<div class="column">
-					<a href="' . $xml2->root[0]->url[$i] . '">
+					<a href="' . $xml2->url[$i] . '">
 						<div class="card">
 							<div class="card-image">
 								<figure class="image is-4by3">
-									<img src="' . $xml2->root[0]->img[$i] . '" alt="Placeholder image">
+									<img src="' . $xml2->img[$i] . '" alt="Placeholder image">
 								</figure>
 							</div>
 							<div class="card-content">
 								<div class="media">
 									<div class="media-left">
 										<figure class="image is-48x48">
-											<img src="' . $xml2->root[0]->img[$i] . '" alt="Placeholder image">
+											<img src="' . $xml2->img[$i] . '" alt="Placeholder image">
 										</figure>
 									</div>
 									<div class="media-content">
-										<p class="title is-4">' . $xml2->root[0]->title[$i] . '</p>
-										<p class="subtitle is-6">' . $xml2->root[0]->location[$i] . ', ' . $xml2->root[0]->age[$i] . '</p>
+										<p class="title is-4">' . $xml2->title[$i] . '</p>
+										<p class="subtitle is-6">' . $xml2->location[$i] . ', ' . $xml2->age[$i] . '</p>
 									</div>
 								</div>
 							</div>
@@ -38,23 +37,23 @@ while($i < 100){
 				<div class="columns is-desktop">';
 }else{
 	$middle .= '<div class="column">
-					<a href="' . $xml2->root[0]->url[$i] . '">
+					<a href="' . $xml2->url[$i] . '">
 						<div class="card">
 							<div class="card-image">
 								<figure class="image is-4by3">
-									<img src="' . $xml2->root[0]->img[$i] . '" alt="Placeholder image">
+									<img src="' . $xml2->img[$i] . '" alt="Placeholder image">
 								</figure>
 							</div>
 							<div class="card-content">
 								<div class="media">
 									<div class="media-left">
 										<figure class="image is-48x48">
-											<img src="' . $xml2->root[0]->img[$i] . '" alt="Placeholder image">
+											<img src="' . $xml2->img[$i] . '" alt="Placeholder image">
 										</figure>
 									</div>
 									<div class="media-content">
-										<p class="title is-4">' . $xml2->root[0]->title[$i] . '</p>
-										<p class="subtitle is-6">' . $xml2->root[0]->location[$i] . ', ' . $xml2->root[0]->age[$i] . '</p>
+										<p class="title is-4">' . $xml2->title[$i] . '</p>
+										<p class="subtitle is-6">' . $xml2->location[$i] . ', ' . $xml2->age[$i] . '</p>
 									</div>
 								</div>
 							</div>
