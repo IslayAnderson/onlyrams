7aa33e444e6487f423605ce7c063887b2d33f419
MrAnderson64 <mranderson64@live.com>
2020-07-23 02:18:28 +0100

    added section and container

--- templates/page.php
+++ templates/page.php
@@ -1,7 +1,12 @@
 <?php 
-$top = '<h1>' . $contentTitle . '</h1>';
+$top = '
+			<section class="section">
+				<div class="container"><h1>' . $contentTitle . '</h1>';
 $middle = $xml->content ;
-$bottom = '';
+$bottom = '
+				</div>
+			</section>
+			';
 
 $templateOutput = $top . $middle . $bottom;
 	
