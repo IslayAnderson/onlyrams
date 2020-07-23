b7d87e276f13ab62625823cf06123951b6f81a41
MrAnderson64 <mranderson64@live.com>
2020-07-23 06:01:23 +0100

    added built by in footer

--- foot.php
+++ foot.php
@@ -6,6 +6,9 @@
   <p class="level-item has-text-centered">
     <a href="/contact-us" class="link is-info">Contact</a>
   </p>
+  <p class="level-item has-text-centered">
+    <a href="https://liamanderson.co.uk" class="link is-info">Built and maintained by liam Anderson</a>
+  </p>
   </div>
 </footer>
 	<script src="<?php echo $host ?>/scripts/functions.js" rel="text/javascript"></script> <!-- Functions.js -->
